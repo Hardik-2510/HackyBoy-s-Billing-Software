434295f2cf8dcc8fb20ff96a59c66bb3c7fd7f04
Hardik Patel <hardik25patel05@gmail.com>
2026-04-11 00:02:54 +0530

    Methods Are Generates Only

---
+++ DAL/DeaCustDAL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Text;
+
+namespace Billing_Software.DAL
+{
+    internal class DeaCustDAL
+    {
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
+        #region Select Method For Dealer/Customer
+
+        public DataTable Select()
+        {
+            DataTable dt = new DataTable();
+
+
+            return dt;
+        }
+
+        #endregion
+        #region Insert Method For Dealer/Customer
+        #endregion
+        #region Update Method For Dealer/Customer
+        #endregion
+        #region Delete Method For Dealer/Customer
+        #endregion
+    }
+}
